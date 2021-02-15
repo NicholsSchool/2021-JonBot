e8dde0a5b18ae0d9d8be796e118b664a668ddb16
Camden Bartlo <camdenbartlo@Camdens-MacBook-Air.local>
2021-02-15 16:42:48 -0500

    Vendor libraries, aye?

---
+++ src/main/java/frc/robot/subsystems/DriveTrain.java
@@ -0,0 +1,9 @@
+package frc.robot.subsystems;
+
+import com.ctre.phoenix.motorcontrol.can.WPI_TalonSRX;
+
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+
+public class DriveTrain extends SubsystemBase {
+    
+}
